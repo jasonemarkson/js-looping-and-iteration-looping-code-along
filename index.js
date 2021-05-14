2c7136ac26eaa9e5403af834a1ad9220494460a4
jasonemarkson <jasonmarkson76@gmail.com>
2021-05-13 20:21:07 -0400

    commit tree 14ee9e7060fc2ecc8bbbf5499ee9ae52de11a84f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array) {
+    let notes = []
+    for (let i = 0; i < array.length; i++) {
+        notes.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return notes
+}
+
+function countDown() {
+    let i = 10;
+    while (i > -1) {
+        console.log(i)
+        i--
+    }
+}
